328e61f65ddace4f3f7ea825c69d859d7da87aa8
Danila <dakfadeev@gmail.com>
2021-10-16 20:11:00 +0300

    closed #1

--- routers/add_text.py
+++ routers/add_text.py
@@ -1,6 +1,8 @@
 import os
 
 from docx import Document
+from docx.shared import Pt
+from docx.enum.style import WD_STYLE_TYPE
 from fastapi import APIRouter, Cookie
 from fastapi.responses import RedirectResponse
 from typing import Optional
@@ -29,13 +31,38 @@ async def add_text(main_theme: str, arg: str, themes: str, link: str, auth_psw: 
             name = "1.docx"
         else:
             name = str(int(texts[len(texts) - 2].split(".")[0]) + 1) + ".docx"
-        document.add_paragraph(arg)
+        p = document.add_paragraph()
+        font_styles = document.styles
+        font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
+        font_object = font_charstyle.font
+        font_object.size = Pt(14)
+        font_object.name = 'Times New Roman'
+        p.add_run(arg, style='CommentsStyle')
         document.save(f'{path}/{name}')
+
         document = Document()
-        document.add_paragraph(f"Количество слов - {len(arg.split(' '))}\nОсновная тематика - {main_theme}\nСмежные "
-                               f"тематики - {themes}\nИсточник - {link}")
+        p = document.add_paragraph()
+        font_styles = document.styles
+        font_charstyle = font_styles.add_style('CommentsStyle', WD_STYLE_TYPE.CHARACTER)
+        font_object = font_charstyle.font
+        font_object.size = Pt(14)
+        font_object.name = 'Times New Roman'
+        p.add_run(f"Количество слов - {len(arg.split(' '))}\nОсновная тематика - {main_theme}\nСмежные"
+                  f" тематики - {themes}\nИсточник - {link}", style='CommentsStyle')
         document.save(f'{path}/Справочные карточки/Справочная карточка_{name}')
-        return RedirectResponse(f"/files/7%20сем/Информационно-поисковые%20системы/300%20текстов", status_code=302)
+        return RedirectResponse(f"/files/7%20сем/Информационно-поисковые%20системы", status_code=302)
+    except AttributeError:
+        return show_forbidden_page()
+    except Exception as er:
+        print(er)
+
+
+@router.get("/check/")
+async def check_text(arg: str, auth_psw: Optional[str] = Cookie(None)):
+    try:
+        if not is_authorized_user(auth_psw):
+            return show_forbidden_page()
+        return f"Words count: {len(arg.split(' '))}"
     except AttributeError:
         return show_forbidden_page()
     except Exception as er:

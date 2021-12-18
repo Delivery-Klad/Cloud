d316974261241dd5f948b72a192716ed59015216
Danila <dakfadeev@gmail.com>
2021-12-19 01:19:24 +0300

    auth alerts + start context folders

--- source/auth.js
+++ source/auth.js
@@ -1,30 +1,36 @@
+var alert_box = document.getElementById("alert-box");
+
+function create_message(text){
+    var block = document.createElement("div");
+    block.setAttribute("style", "background-color: #D70000; margin: 5px; padding: 10px; font: 12pt sans-serif;");
+    block.textContent = text;
+    alert_box.appendChild(block);
+    setTimeout(remove_message, 5000, block);
+}
+
 function login(){
     var user_login = document.getElementById("login");
     var user_password = document.getElementById("password");
     for (let chr of user_login.value) {
         if (123 < chr.charCodeAt(0) || 33 > chr.charCodeAt(0)) {
-            alert("Использованы неподдерживаемые символы");
+            create_message("Использованы неподдерживаемые символы");
             return;
         }
     }
     if (user_login.value === "user"){
-        alert("User не может быть использовано в качестве логина");
+        create_message("User не может быть использовано в качестве логина");
         return;
     }
-    if (user_login.value === ""){
-        alert("Не все поля заполнены");
-        return;
-    }
-    if (user_login.value === ""){
-        alert("Не все поля заполнены");
+    if (user_login.value === "" || user_password.value === ""){
+        create_message("Не все поля заполнены");
         return;
     }
     if (user_login.value.length < 4){
-        alert("Логин слишком короткий");
+        create_message("Логин слишком короткий");
         return;
     }
     if (user_password.value.length < 8){
-        alert("Пароль слишком короткий");
+        create_message("Пароль слишком короткий");
         return;
     }
     var xhr = new XMLHttpRequest();
@@ -40,10 +46,14 @@ function login(){
             document.location.href = redirect;
         }
         if(xhr.readyState === 4 && xhr.status === 403){
-            alert(JSON.parse(xhr.responseText).result);
+            create_message(JSON.parse(xhr.responseText).result);
         }
     }
-    xhr.send()
+    xhr.send();
+}
+
+function remove_message(block){
+    block.remove();
 }
 
 $("form").on('submit', function (e) {
--- source/context.js
+++ source/context.js
@@ -1,70 +1,75 @@
 <script type="text/javascript">
 var place_holder = document.getElementById("meta_place_holder")
+
 $(document).ready(function() {
-
-  if ($("#file").addEventListener) {
-    $("#file").addEventListener('contextmenu', function(e) {
-      alert("You've tried to open context menu");
-      e.preventDefault();
-    }, false);
-  } else {
-
-    $('body').on('contextmenu', 'a.file', function() {
-      var xhr = new XMLHttpRequest();
-      var text = $(this).contents()[0].nodeValue.trim();
-      xhr.open('GET', '/file/meta?path=' + document.getElementById("file_path").value + '&name=/' + text);
-      xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4 && xhr.status === 200){
-			var arr = JSON.parse(xhr.responseText).res;
-            place_holder.textContent = "";
-            arr.forEach((element) => {
-                let block = document.createElement('div');
-                block.textContent += element;
-                place_holder.append(block);
-            })
-        }
-      }
-      xhr.send()
-      document.getElementById("menu_m").className = "show";
-      document.getElementById('file_name').value = text;
-      document.getElementById("menu_m").style.top = mouseY(event) + 'px';
-      document.getElementById("menu_m").style.left = mouseX(event) + 'px';
-      setFocusToTextBox();
-      window.event.returnValue = false;
-    });
-  }
+    if ($("#file").addEventListener) {
+        $("#file").addEventListener('contextmenu', function(e) {
+            alert("You've tried to open context menu");
+            e.preventDefault();
+        }, false);
+    } else {
+        $('body').on('contextmenu', 'a.file', function() {
+            var xhr = new XMLHttpRequest();
+            var text = $(this).contents()[0].nodeValue.trim();
+            xhr.open('GET', '/file/meta?path=' + document.getElementById("file_path").value + '&name=/' + text);
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState === 4 && xhr.status === 200){
+                    var arr = JSON.parse(xhr.responseText).res;
+                    place_holder.textContent = "";
+                    arr.forEach((element) => {
+                        let block = document.createElement('div');
+                        block.textContent += element;
+                        place_holder.append(block);
+                    })
+                }
+            }
+            xhr.send()
+            document.getElementById("menu_m").className = "show";
+            document.getElementById('file_name').value = text;
+            document.getElementById("menu_m").style.top = mouseY(event) + 'px';
+            document.getElementById("menu_m").style.left = mouseX(event) + 'px';
+            setFocusToTextBox();
+            window.event.returnValue = false;
+        });
+        //$('body').on('contextmenu', 'a.folder', function() {
+            //place_holder.textContent = "";
+            //document.getElementById("menu_m").className = "show";
+            //document.getElementById('file_name').value = text;
+            //document.getElementById("menu_m").style.top = mouseY(event) + 'px';
+            //document.getElementById("menu_m").style.left = mouseX(event) + 'px';
+            //window.event.returnValue = false;
+        //});
+    }
 });
 
 $(document).bind("click", function(event) {
-    if (event.target.name === "new_name"){
-        return;
-    }
+    if (event.target.name === "new_name"){ return; }
     place_holder.textContent = "";
     document.getElementById("menu_m").className = "hide";
 });
 
 function mouseX(evt) {
-  if (evt.pageX) {
-    return evt.pageX;
-  } else if (evt.clientX) {
-    return evt.clientX + (document.documentElement.scrollLeft ?
-      document.documentElement.scrollLeft :
-      document.body.scrollLeft);
-  } else {
-    return null;
-  }
+    if (evt.pageX) {
+        return evt.pageX;
+    } else if (evt.clientX) {
+        return evt.clientX + (document.documentElement.scrollLeft ?
+            document.documentElement.scrollLeft :
+            document.body.scrollLeft);
+    } else {
+        return null;
+    }
 }
 
 function mouseY(evt) {
-  if (evt.pageY) {
-    return evt.pageY;
-  } else if (evt.clientY) {
-    return evt.clientY + (document.documentElement.scrollTop ?
-      document.documentElement.scrollTop :
-      document.body.scrollTop);
-  } else {
-    return null;
-  }
+    if (evt.pageY) {
+        return evt.pageY;
+    } else if (evt.clientY) {
+        return evt.clientY + (document.documentElement.scrollTop ?
+            document.documentElement.scrollTop :
+            document.body.scrollTop);
+    } else {
+        return null;
+    }
 }
 
 function setFocusToTextBox(){

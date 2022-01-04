e1bd15f0c5c0667a52d4f2722f350ade4dd9bf83
Danila <dakfadeev@gmail.com>
2022-01-04 18:37:08 +0300

    closed #32

---
+++ source_admin/replace.js
@@ -0,0 +1,15 @@
+function replace_file(old_path, new_path){
+    var data = {};
+    data.old_path = old_path;
+    data.new_path = new_path;
+    var json = JSON.stringify(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open("PATCH", "/file/", true);
+    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+    xhr.onload = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.location.href = new_path;
+        }
+    }
+    xhr.send(json);
+}

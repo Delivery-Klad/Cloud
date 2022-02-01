3f0f04c4a5922acaec743a11d19ab9690561a417
Danila <dakfadeev@gmail.com>
2022-02-02 01:03:15 +0300

    remove useless test

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,9 +9,3 @@ client = TestClient(app)
 def test_get_files():
     response = client.get("/files/")
     assert response.status_code == 404
-
-
-def test_get_controller():
-    response = client.get("/test/")
-    assert response.status_code == 200
-    assert response.json() == 0
